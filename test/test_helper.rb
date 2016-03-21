91acf040cab0aa87eaedb9640a4a76e7eed587fc
Steve Pentler <stevepentler@gmail.com>
2016-03-21 14:01:40 -0600

    Adds FactoryGirl and Database Cleaner to test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,8 +5,10 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  include FactoryGirl::Syntax::Methods
 
-  # Add more helper methods to be used by all tests here...
+  def teardown 
+    DatabaseCleaner.clean
+  end
+
 end
