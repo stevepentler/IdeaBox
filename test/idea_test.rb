7ed9adb66b50e196a0fcef506771c385f7072a96
Steve Pentler <stevepentler@gmail.com>
2016-03-21 13:53:49 -0600

    Adds idea model test, idea.quality defaults to swill

---
+++ test/idea_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class IdeaTest < ActiveSupport::TestCase
+  test 'quality defaults to swill' do 
+    idea = Idea.create(title: "Hotdogs",
+                       body: "roller")
+
+    assert_equal "swill", idea.quality
+    assert_equal "Hotdogs", idea.title
+    assert_equal "roller", idea.body
+  end
+end
