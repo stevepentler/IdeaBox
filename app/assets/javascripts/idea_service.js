dd37585ef05ed29850040679461595ca2128b210
Steve Pentler <stevepentler@gmail.com>
2016-03-21 16:45:00 -0600

    Displays index without styling

--- app/assets/javascripts/idea_service.js
+++ app/assets/javascripts/idea_service.js
@@ -1,18 +1,33 @@
-$(document).ready(function() {
-  function IdeaDisplay(idea) {
+$(document).ready(function(){
+
+  function renderIdea(idea){
+    console.log('steve');
     $('#ideas-index').append(
-      "<div data-id='" + idea.id +"'><h6>Published on: " +
+      "<div class='idea' data-id='" +
+      idea.id +
+      "'><h6>Published on: " +
       idea.created_at +
       "</h6><p>" +
-      idea.description +
+      idea.body +
       "</p>" +
-      "<button id='delete-idea' name='button-fetch' class='btn btn-default btn-xs'>Delete</button>"+"</div>"
-    )
+      "<button id='delete-idea' name='button-fetch' class='btn btn-default btn-xs'>Delete</button>" +
+      "</div>"
+    );
   }
-  $.getJSON('/api/v1/ideas', ideas) {
-    $.each(ideas, function(index, idea){
-      IdeaDisplay(idea)
+
+  function getIdeas(){
+    console.log('steve2')
+    $.getJSON('/api/v1/ideas', function(ideas){
+      $.each(ideas, function(index, idea){
+        renderIdea(idea);
+      })
     })
-  }
-  
+  };
+
+  getIdeas()
+
+  $('#fetch-ideas').click(function() {
+    getIdeas()
+  });
+
 })
