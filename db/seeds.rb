217984c9e061ef5c40cd68cde0e3d03d19f599a4
Steve Pentler <stevepentler@gmail.com>
2016-03-21 14:34:28 -0600

    Adds 10 ideas for each quality level to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,20 @@
 10.times do |i|
   Idea.create(
     title: "Title#{i}",
-    body: "Body#{i}")
+    body: "Body#{i}",
+    quality: 0)
+end
+
+10.times do |i|
+  Idea.create(
+    title: "Title#{i + 10}",
+    body: "Body#{i + 10}",
+    quality: 1)
+end
+
+10.times do |i|
+  Idea.create(
+    title: "Title#{i + 20}",
+    body: "Body#{i + 20}",
+    quality: 2)
 end
