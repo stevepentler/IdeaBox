304945b1c79dce07efd119c89815c10674204c55
Steve Pentler <stevepentler@gmail.com>
2016-03-22 21:42:39 -0600

    Search functionality with jQuery

--- app/assets/javascripts/idea_service.js
+++ app/assets/javascripts/idea_service.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
   deleteIdea();
   promoteIdea();
   demoteIdea();
+  searchIdeas();
 });
 
 
@@ -193,6 +194,22 @@ console.log("demoted quality = " + newQuality);
         }
       });
       
+    });
+  }
+
+  function searchIdeas() {
+    $("#search").keyup(function() {
+      var entry = $(this).val().toLowerCase();
+      var ideasIndex = $('#ideas-index').children();
+      $.each(ideasIndex, function(index, idea){
+        var title = $(idea).find('.title').text().toLowerCase();
+        var body = $(idea).find('.body').text().toLowerCase();
+        if (title.includes(entry) || body.includes(entry)) {
+          $(idea).show();
+        } else {
+          $(idea).hide();
+        }
+      });
     });
   }
 
