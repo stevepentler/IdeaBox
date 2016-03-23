66564a5dc7eb398af29ff9f1555c722d485f6b16
Steve Pentler <stevepentler@gmail.com>
2016-03-23 14:02:23 -0600

    abstracts qualityStatus and qualityCall from qualityIdea function

--- app/assets/javascripts/quality_idea.js
+++ app/assets/javascripts/quality_idea.js
@@ -2,39 +2,37 @@ function qualityIdea(status) {
   $('#ideas-index').delegate(status, 'click', function() {
     var $idea = $(this).closest('.idea');
     var previousQuality = $idea.find('p').text().split(" ")[1];
-
-    var qualityStatus = function() {
-      if (status == "#demote-button") {
-        return demoteQuality(previousQuality);
-      } else {
-        return promoteQuality(previousQuality);
-      }
-    };
-
+    var newQuality = qualityStatus(status, previousQuality);
     var ideaParams = {
       idea: {
-        quality: qualityStatus
+        quality: newQuality
       }
     };
-
-    qualityCall($idea, ideaParams, qualityStatus);
-
+    qualityCall($idea, ideaParams, newQuality);
   });
 }
 
-function qualityCall(idea, ideaParams, qualityStatus) {
+function qualityCall(idea, ideaParams, newQuality) {
   $.ajax({
     type: "PUT",
     url: "/api/v1/ideas/" + idea.attr('idea-id'),
     data: ideaParams,
     success: function() {
-      quality = qualityStatus;
-      idea.find('p').text("Quality: " + quality());
+      quality = newQuality;
+      idea.find('p').text("Quality: " + newQuality);
     },
     error: function(xhr) {
       console.log(xhr.responseText);
     }
   });
+}
+
+function qualityStatus(status, previousQuality) {
+  if (status == "#demote-button") {
+    return demoteQuality(previousQuality);
+  } else {
+    return promoteQuality(previousQuality);
+  }
 }
 
 function demoteQuality(previousQuality) {
