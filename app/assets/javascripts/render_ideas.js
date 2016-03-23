76747c56c2af2fe076e37fd3f7cbbd9c683cdfad
Steve Pentler <stevepentler@gmail.com>
2016-03-22 23:24:05 -0600

    Formats date with date function in render_idea.js

--- app/assets/javascripts/render_ideas.js
+++ app/assets/javascripts/render_ideas.js
@@ -1,15 +1,19 @@
 function renderIdea(idea){
   $('#ideas-index').prepend(
-    "<div class='idea' idea-id='" +
-    idea.id + "'><h6>Published on: " +
-    idea.created_at +
-    "<h5 class='title'>" + idea.title + "</h5><h6 class='body'>" +
-    truncate(idea.body) +
-    "</h6><p>Quality: " + idea.quality +
-    "</p><button id='delete-button' class='btn btn-default btn-xs'>Delete</button>" +
-    "<button id='promote-button' class='btn btn-default btn-xs'>Promote</button>" +
-    "<button id='demote-button' class='btn btn-default btn-xs'>Demote</button>" +
-    "<button id='edit-button' class='btn btn-default btn-xs'>Edit</button>" +
+    "<div class='idea' idea-id='" + idea.id + "'>" +
+    
+      "<h5 class='title'>" + idea.title + "</h5><h6 class='body'>" +
+      "<h6>Published on: " + date(idea) + "</h6>" + "<br>" +
+      truncate(idea.body) +
+      "</h6><p>Quality: " + idea.quality +
+      "</p><button id='delete-button' class='btn btn-default btn-xs'>Delete</button>" +
+      "<button id='promote-button' class='btn btn-default btn-xs'>Promote</button>" +
+      "<button id='demote-button' class='btn btn-default btn-xs'>Demote</button>" +
+      "<button id='edit-button' class='btn btn-default btn-xs'>Edit</button>" +
     "</div>"
   );
+}
+
+function date(idea) {
+  return idea.created_at.split("T")[0];
 }
