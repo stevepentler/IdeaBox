007388cce36b9daad9843a977645102dd4e9752f
Steve Pentler <stevepentler@gmail.com>
2016-03-23 13:50:33 -0600

    Abstracts ajax from deleteIdea to deleteCall

--- app/assets/javascripts/delete_idea.js
+++ app/assets/javascripts/delete_idea.js
@@ -1,16 +1,19 @@
 function deleteIdea() {
   $('#ideas-index').delegate("#delete-button", 'click', function() {
     var $idea = $(this).closest(".idea");
+    deleteCall($idea);
+  });
+}
 
-    $.ajax({
-      type: 'DELETE',
-      url: '/api/v1/ideas/' + $idea.attr('idea-id'),
-      success: function() {
-        $idea.remove();
-      },
-      error: function(xhr) {
-        console.log(xhr.responseText);
-      }
-    });
+function deleteCall(idea) {
+  $.ajax({
+    type: 'DELETE',
+    url: '/api/v1/ideas/' + idea.attr('idea-id'),
+    success: function() {
+      idea.remove();
+    },
+    error: function(xhr) {
+      console.log(xhr.responseText);
+    }
   });
 }
