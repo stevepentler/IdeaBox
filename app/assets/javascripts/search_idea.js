98fdd2d9fa62f8f64fc82a7af8d8c288e0bbceca
Steve Pentler <stevepentler@gmail.com>
2016-03-22 22:44:06 -0600

    Abstracts logic from searchIdeas function

--- app/assets/javascripts/search_idea.js
+++ app/assets/javascripts/search_idea.js
@@ -2,14 +2,22 @@ function searchIdeas() {
   $("#search").keyup(function() {
     var entry = $(this).val().toLowerCase();
     var ideasIndex = $('#ideas-index').children();
-    $.each(ideasIndex, function(index, idea){
-      var title = $(idea).find('.title').text().toLowerCase();
-      var body = $(idea).find('.body').text().toLowerCase();
-      if (title.includes(entry) || body.includes(entry)) {
-        $(idea).show();
-      } else {
-        $(idea).hide();
-      }
-    });
+    limitSearch(ideasIndex, entry);
   });
+}
+
+function limitSearch(ideasIndex, entry) {
+  $.each(ideasIndex, function(index, idea){
+    var title = $(idea).find('.title').text().toLowerCase();
+    var body = $(idea).find('.body').text().toLowerCase();
+    showIdeas(idea, title, body, entry);
+  });
+}
+
+function showIdeas(idea, title, body, entry) {
+  if (title.includes(entry) || body.includes(entry)) {
+    $(idea).show();
+  } else {
+    $(idea).hide();
+  }
 }
